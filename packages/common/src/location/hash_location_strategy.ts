3ec88d94e5ea83d5710399b09744c684c2bb8497
Yinyin L <yinyinl@gmail.com>
2018-11-04 11:18:45 +0000

    feat(router): able to provide base URL with APP_BASE_HREF
    Allows HashLocationStrategy to generate external URL with given
    APP_BASE_HREF. This PR should resolve the deploying host page and rest
    static files separately issue mentioned in #13482 and #20033.
    
    Modified HashLocationStrategy will take base URL with the following order:
    
    * APP_BASE_HREF
    * base-href
    * path of current URL
    
    The HashLocationStrategy used to push only hash part (eg: #/ui/path) to
    history API. This will make browser change the host page part of URL in
    address bar to path pointed by base-href instead of path of host page.
    
    With this patch, the HashLocationStrategy will push both path and hash
    part to history API. In this way the browser will be able to keep the host
    page part of URL in address bar to the path of host page.

--- packages/common/src/location/hash_location_strategy.ts
+++ packages/common/src/location/hash_location_strategy.ts
@@ -25,6 +25,17 @@ import {LocationChangeListener, PlatformLocation} from './platform_location';
  * For instance, if you call `location.go('/foo')`, the browser's URL will become
  * `example.com#/foo`.
  *
+ * You can provide a {@link APP_BASE_HREF} or add a base element to the document.
+ * This URL prefix that will be preserved when generating and recognizing URLs.
+ *
+ * For example, if you provide an `APP_BASE_HREF` of `'/my/app'` and call
+ * `location.go('/foo')`, the browser's URL will become
+ * `example.com/my/app#/foo`.
+ *
+ * Similarly, if you add `<base href='/my/app'/>` to the document and call
+ * `location.go('/foo')`, the browser's URL will become
+ * `example.com/my/app#/foo`.
+ *
  * @usageNotes
  *
  * ### Example
@@ -35,14 +46,16 @@ import {LocationChangeListener, PlatformLocation} from './platform_location';
  */
 @Injectable()
 export class HashLocationStrategy extends LocationStrategy {
-  private _baseHref: string = '';
+  private _baseHref: string;
   constructor(
       private _platformLocation: PlatformLocation,
       @Optional() @Inject(APP_BASE_HREF) _baseHref?: string) {
     super();
-    if (_baseHref != null) {
-      this._baseHref = _baseHref;
+    if (_baseHref == null) {
+      _baseHref = this._platformLocation.getBaseHrefFromDOM();
     }
+    _baseHref = _baseHref || this._platformLocation.pathname;
+    this._baseHref = _stripOnlyIndexHtml(_baseHref);
   }
 
   onPopState(fn: LocationChangeListener): void {
@@ -50,7 +63,7 @@ export class HashLocationStrategy extends LocationStrategy {
     this._platformLocation.onHashChange(fn);
   }
 
-  getBaseHref(): string { return this._baseHref; }
+  getBaseHref(): string { return this._baseHref + '#'; }
 
   path(includeHash: boolean = false): string {
     // the hash value is always prefixed with a `#`
@@ -62,8 +75,11 @@ export class HashLocationStrategy extends LocationStrategy {
   }
 
   prepareExternalUrl(internal: string): string {
-    const url = Location.joinWithSlash(this._baseHref, internal);
-    return url.length > 0 ? ('#' + url) : url;
+    if (internal.length === 0) {
+      return this._baseHref + '#/';
+    }
+    const mark = internal.startsWith('/') ? '#' : '#/';
+    return this._baseHref + mark + internal;
   }
 
   pushState(state: any, title: string, path: string, queryParams: string) {
@@ -86,4 +102,8 @@ export class HashLocationStrategy extends LocationStrategy {
   forward(): void { this._platformLocation.forward(); }
 
   back(): void { this._platformLocation.back(); }
+}
+
+function _stripOnlyIndexHtml(url: string): string {
+  return url.replace(/\/index.html$/, '/');
 }
